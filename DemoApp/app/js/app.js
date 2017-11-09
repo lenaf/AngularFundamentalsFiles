bbe40297f76cd7fd6f4b427cd87d541ca78b8c65
Lena Forti <lena.forti@liazon.com>
2017-11-09 15:00:33 -0500

    inital commit

---
+++ DemoApp/app/js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+//create a module called eventsApp
+//store the module in a global variable to make it easier to create objects inside of module such as controllers and services. 
+var eventsApp = angular.module('eventsApp', []);
